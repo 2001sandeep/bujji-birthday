42c2d5f74f34c38cf398726c107afc8a60797ba1
2001sandeep <91928755+2001sandeep@users.noreply.github.com>
2026-06-04 01:08:57 +0530

    Update script.js

--- script.js
+++ script.js
@@ -378,3 +378,13 @@ function showSecret(){
   .classList.remove("hidden");
 
 }
+function showFinale(){
+
+  document.querySelectorAll(".screen").forEach(screen=>{
+    screen.classList.add("hidden");
+  });
+
+  document.getElementById("finale")
+  .classList.remove("hidden");
+
+}
